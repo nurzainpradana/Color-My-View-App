8062350f0b84e0ed03aa2899c6492d43aa432119
nurzainpradana <pradananurzain@gmail.com>
2020-10-26 15:43:43 +0700

    challenge: ketika di klik, ganti background dengan gambar

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -34,11 +34,11 @@ class MainActivity : AppCompatActivity() {
     private fun makeColored(view: View){
         when (view.id) {
             // Boxes using Color Class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.constraint_layout -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.gambar_satu)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.gambar_dua)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.gambar_tiga)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.gambar_empat)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.gambar_lima)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
 
