456508eb5fd6c4dd4679f03452331eaf1f3b5ab6
nurzainpradana <pradananurzain@gmail.com>
2020-10-27 15:25:28 +0700

    set constraint bias

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -10,34 +10,41 @@ import android.widget.TextView
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    // Inisialisasi Button
     lateinit var redButton: Button
     lateinit var yellowButton: Button
     lateinit var greenButton: Button
 
+    // Inisialisasi TextView
     lateinit var boxOneText: TextView
     lateinit var boxTwoText: TextView
     lateinit var boxThreeText: TextView
     lateinit var boxFourText: TextView
     lateinit var boxFiveText: TextView
 
+    // Inisialisasi CheckBox
     lateinit var checkBoxOne: CheckBox
     lateinit var checkBoxTwo: CheckBox
     lateinit var checkBoxThree: CheckBox
     lateinit var checkBoxFour: CheckBox
     lateinit var checkBoxFive: CheckBox
 
+    // Inisialisasi Layout
     lateinit var rootConstraintLayout: View
 
+    // Inisialisasi Array List
     lateinit var selectedBox : ArrayList<View>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Set Listener
         setListener()
     }
 
     private fun setListener() {
+        // Mencari view berdasarkan ID
         boxOneText = findViewById(R.id.box_one_text)
         boxTwoText = findViewById(R.id.box_two_text)
         boxThreeText = findViewById(R.id.box_three_text)
@@ -48,14 +55,15 @@ class MainActivity : AppCompatActivity() {
         redButton = findViewById(R.id.red_button)
         yellowButton = findViewById(R.id.yellow_button)
 
-        checkBoxOne = findViewById(R.id.checkBoxOne)
-        checkBoxTwo = findViewById(R.id.checkBoxTwo)
-        checkBoxThree = findViewById(R.id.checkBoxThree)
-        checkBoxFour = findViewById(R.id.checkBoxFour)
-        checkBoxFive = findViewById(R.id.checkBoxFive)
+        checkBoxOne = findViewById(R.id.check_box_one)
+        checkBoxTwo = findViewById(R.id.check_box_two)
+        checkBoxThree = findViewById(R.id.check_box_three)
+        checkBoxFour = findViewById(R.id.check_box_four)
+        checkBoxFive = findViewById(R.id.check_box_five)
 
         rootConstraintLayout = findViewById(R.id.constraint_layout)
 
+        // Menampung view
         val clickableViews: List<View> =
             listOf(boxOneText,
                 boxTwoText,
@@ -71,6 +79,7 @@ class MainActivity : AppCompatActivity() {
             item.setOnClickListener { makeDrawabled(it) }
         }
 
+        // Membuat Array List
         selectedBox = ArrayList()
     }
 
@@ -83,13 +92,14 @@ class MainActivity : AppCompatActivity() {
             R.id.box_four_text -> view.setBackgroundResource(R.drawable.gambar_empat)
             R.id.box_five_text -> view.setBackgroundResource(R.drawable.gambar_lima)
 
-            R.id.yellow_button -> makeCheckBoxColored(Color.YELLOW)
-            R.id.red_button -> makeCheckBoxColored(Color.RED)
-            R.id.green_button -> makeCheckBoxColored(Color.GREEN)
+            R.id.yellow_button -> makeColoredByCheckBox(Color.YELLOW)
+            R.id.red_button -> makeColoredByCheckBox(Color.RED)
+            R.id.green_button -> makeColoredByCheckBox(Color.GREEN)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
-
+    /*
     private fun makeColored(view: View){
         when (view.id) {
             // Boxes using Color Class colors for the background
@@ -103,7 +113,10 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+     */
+
     fun onCheckBoxClicked(view: View) {
+        // Action ketika checkbox di klik
         if(view is CheckBox){
             when(view){
                 checkBoxOne -> checkAndAddToArrayList(checkBoxOne, boxOneText)
@@ -116,6 +129,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun checkAndAddToArrayList(checkBox : CheckBox, view: View){
+        // Mengecek apakah checked atau tidak
+        // Jika checked akan ditambahkan ke arrayList selectedBox
+        // Jika tidak, maka tidak akan ditambahkan / dihapus dari arrayList
         val isChecked: Boolean = checkBox.isChecked
         if (isChecked){
             selectedBox.add(view)
@@ -124,7 +140,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun makeCheckBoxColored(color: Int){
+    fun makeColoredByCheckBox(color: Int){
+        // Mengganti background view yang ada di arrayList selectedBox berdasarkan warna yang dikirim pada parameter
         for (item in selectedBox){
             item.setBackgroundColor(color)
         }
